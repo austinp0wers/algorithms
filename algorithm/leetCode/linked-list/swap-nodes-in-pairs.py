da64145d2770cad5fd266df2493b75fc3154e394
Taegyun Austin Lee <95058190+austinp0wers@users.noreply.github.com>
2022-09-17 14:07:39 +0900

    Update swap-nodes-in-pairs.py

--- algorithm/leetCode/linked-list/swap-nodes-in-pairs.py
+++ algorithm/leetCode/linked-list/swap-nodes-in-pairs.py
@@ -1,5 +1,7 @@
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # i've originally wanted to only change the links between the nodes but that didn't work.
+        # so this is what i've done. I was NOT satisfied
         temp = []
         while head:
             temp.append(head.val)
@@ -14,3 +16,15 @@ class Solution:
             curr = curr.next 
         return dummy.next
             
+
+# this is what i've seen someone do. 
+# instead of changing the links, you swap the val of the nodes.
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        cur = head
+        while head and head.next:
+            head.val, head.next.val = head.next.val, head.val
+            head = head.next.next
+            
+        return cur
